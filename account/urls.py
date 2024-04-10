cda8d24ed0568987b6e4823543b75ef58342037d
astrophilesara <161948803+astrophilesara@users.noreply.github.com>
2024-04-10 18:24:55 +0330

    complete login form

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 
+app_name = "account"
 
 urlpatterns = [
-    path("login/", views.login),
+    path("login/", views.login_user, name = "login"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
-def login(request):
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            pass
+  
     return render(request, "account/login.html", context={})
