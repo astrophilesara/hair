3bb75ef855459f4b8fa5330157848205bc45593b
astrophilesara <161948803+astrophilesara@users.noreply.github.com>
2024-04-15 00:49:49 +0330

    navigation changes

--- us/views.py
+++ us/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request, "us/about.html", context={})
+
+
+
+# def contact(request):
+#     return render(request, "us/contact.html", context={})
